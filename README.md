af25706fa76c91e78dfebb6c8c1ac4e8c33ee1a6
ryodgie <ryodgie@gmail.com>
2023-03-12 11:02:58 +0800

    feat: complete qr code component

---
+++ README.md
@@ -0,0 +1,12 @@
+# QR code component 🤖
+
+![Design preview for the QR code component coding challenge](./src/assets/desktop-preview.jpg)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [Vue](https://vuejs.org/) - JS library
---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="card">
+        <img
+          class="img-qr-code"
+          src="./assets/image-qr-code.png"
+          alt="qrcode"
+        />
+        <div class="lead-text-container">
+          <h1 class="title">
+            Improve your front-end skills by building projects
+          </h1>
+          <p class="lead-text">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1440px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin: 0 auto;
+  height: 100vh;
+}
+
+.card {
+  background-color: var(--dark);
+  max-width: 19.5rem;
+  justify-content: center;
+  border-radius: 1.35rem;
+  padding: 1rem;
+  box-shadow: 12px 12px 52px #afaeae31, -12px -12px 52px #faf9f905;
+}
+.img-qr-code {
+  border-radius: 0.625rem;
+}
+
+.lead-text-container {
+  padding: 1.75rem 1rem;
+}
+
+.title {
+  color: var(--light-blue);
+  font-size: 1.35rem;
+  font-weight: 700;
+  line-height: 1.65rem;
+}
+
+.lead-text {
+  color: var(--light-gray);
+  font-size: 1rem;
+  font-weight: 400;
+  padding-top: 1rem;
+  line-height: 1.25rem;
+}
+</style>
